8d143b983832718fa1a005076ba51b57bacb9b68
pablodru <pdrocha03@gmail.com>
2023-06-23 18:29:44 -0300

    implementação do get de tweets

--- src/app.js
+++ src/app.js
@@ -6,9 +6,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const users =    [];
+const users =   [{
+                    username: "bobesponja",
+                    avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png"
+                }];
 
-const tweets =  [{
+const allTweets =  [{
 	                username: "bobesponja",
                     tweet: "Eu amo hambúrguer de siri!"
                 }];
@@ -22,18 +25,35 @@ app.post('/sign-up', (req, res) => {
 });
 
 app.post('/tweets', (req, res) => {
-    const { username, tweets } = req.body;
+    const { username, tweet } = req.body;
 
     if( !users.find(u => u.username === username) ){
         return res.status(401).send({message: "Usuário não cadastrado!"});
     }
 
-    const body = { username, tweets };
-    tweets.push(body);
+    const body = { username, tweet };
+    allTweets.push(body);
     res.status(201).send({message: "OK"});
 });
 
+app.get('/tweets', (req, res) => {
+    
+    const arrayTweets = []
 
+    allTweets.forEach(tweet => {
+        let avatar = '';
+        users.find(user => {
+            if(user.username === tweet.username){
+                avatar = user.avatar;
+            }
+        });
+        console.log(tweet);
+        const body = {username: tweet.username, avatar, tweet: tweet.tweet};
+        arrayTweets.push(body);
+    })
+
+    res.status(201).send(arrayTweets.slice(-10));
+})
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
