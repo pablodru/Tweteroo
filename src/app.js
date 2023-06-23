11e07079ff5ae71af92a55b57c7e477a3dddef02
pablodru <pdrocha03@gmail.com>
2023-06-23 19:03:24 -0300

    correção no mock de dados

--- src/app.js
+++ src/app.js
@@ -6,15 +6,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const users =   [{
-                    username: "bobesponja",
-                    avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png"
-                }];
+const users =   [];
 
-const allTweets =  [{
-	                username: "bobesponja",
-                    tweet: "Eu amo hambúrguer de siri!"
-                }];
+const allTweets =  [];
 
 app.post('/sign-up', (req, res) => {
     const { username, avatar } = req.body;
